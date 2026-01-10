48b44c8b20cad4081589e62bf5ff17dfd60cb9d0
nolanflores <nolan.t.flores@gmail.com>
2026-01-09 22:15:17 -0800

    example discord token header

---
+++ discord_token.h
@@ -0,0 +1,8 @@
+#ifndef DISCORD_TOKEN_H
+#define DISCORD_TOKEN_H
+
+//Make sure to leave the "Bot " prefix in the token string
+//Or remove it if you're using a user token (against the Discord TOS)
+#define DISCORD_TOKEN "Bot YOUR_BOT_TOKEN_HERE"
+
+#endif
