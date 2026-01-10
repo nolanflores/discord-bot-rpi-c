bc4d3723e51c3382b988b363469d3708b8d2bff6
nolanflores <nolan.t.flores@gmail.com>
2026-01-09 16:36:56 -0800

    stop tracking discord_token.h

--- discord_token.h
+++
@@ -1,8 +0,0 @@
-#ifndef DISCORD_TOKEN_H
-#define DISCORD_TOKEN_H
-
-//Make sure to leave the "Bot " prefix in the token string.
-//Or remove the prefix if you are using a user token (against the discord TOS).
-#define DISCORD_TOKEN "Bot YOUR_DISCORD_BOT_TOKEN_HERE"
-
-#endif
