5d27127573c87632c9c25a583a7a5ff6206aec0c
nolanflores <nolan.t.flores@gmail.com>
2026-02-18 22:08:05 -0800

    added an argument to the compile script to optimize for the target Raspberry Pi

--- compile.sh
+++ compile.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-gcc -std=gnu11 -Wall -Wextra src/main.c src/core/*.c src/bot/*.c -I include -I ./ -o run -l ssl -l crypto
+
+if [ "$1" = "pi" ]; then
+    OPT="-O2"
+    ARCH="-march=native -mtune=cortex-a53"
+else
+    OPT="-O0"
+    ARCH=""
+fi
+
+gcc -std=gnu11 -Wall -Wextra $OPT $ARCH src/main.c src/core/*.c src/bot/*.c -I include -I ./ -o run -l ssl -l crypto
